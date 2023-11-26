7d30427ee78cc6794b69106edd7febdcaeb72389
chandupaRana <chandupanr03@gmail.com>
2023-11-25 23:18:51 -0500

    register component

--- my-app/src/components/Register.js
+++ my-app/src/components/Register.js
@@ -18,7 +18,7 @@ function Register() {
         }
         setError('');
         try {
-            const response = await fetch('/api/register', {
+            const response = await fetch('http://localhost:3000/users', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, email, password }),
